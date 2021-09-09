62db73a169882d6417aa9576b582771a19b0696d
Bruna Amoretti Guimarães <brubru.amoretti@gmail.com>
2021-09-09 14:17:41 -0300

    Spring test utilizando o JUnit no Back-End do Blog_Pessoal | 09.09.2021

--- Blog_Pessoal/src/main/java/com/example/blogpessoal/model/Usuario.java
+++ Blog_Pessoal/src/main/java/com/example/blogpessoal/model/Usuario.java
@@ -1,8 +1,10 @@
 package com.example.blogpessoal.model;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,6 +14,7 @@ import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
@@ -31,48 +34,75 @@ public class Usuario {
 	@NotNull(message = "O atributo Senha é Obrigatória!")
 	@Size(min = 8, message = "A Senha deve ter no mínimo 8 caracteres")
 	private String senha;
+	
+	@Column(name = "data_nascimento")
+	@JsonFormat(pattern = "yyyy-MM-dd")
+	@NotNull(message = "O atributo Data de Nascimento é Obrigatório!")
+	private LocalDate dataNascimento;
 
 	@OneToMany(mappedBy = "usuario", cascade = CascadeType.REMOVE)
 	@JsonIgnoreProperties("usuario")
 	private List<Postagem> postagem;
-
-	public long getId() {
-		return id;
-	}
-
-	public void setId(long id) {
+	
+public Usuario(long id, String nome, String usuario, String senha, LocalDate dataNascimento) {
+		
 		this.id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
 		this.nome = nome;
-	}
-
-	public String getUsuario() {
-		return usuario;
-	}
-
-	public void setUsuario(String usuario) {
 		this.usuario = usuario;
-	}
-
-	public String getSenha() {
-		return senha;
-	}
-
-	public void setSenha(String senha) {
 		this.senha = senha;
+		this.dataNascimento = dataNascimento;
+		
 	}
 
-	public List<Postagem> getPostagem() {
-		return postagem;
-	}
+public Usuario() { }
 
-	public void setPostagem(List<Postagem> postagem) {
-		this.postagem = postagem;
-	}
+
+public long getId() {
+	return id;
+}
+
+public void setId(long id) {
+	this.id = id;
+}
+
+public String getNome() {
+	return nome;
+}
+
+public void setNome(String nome) {
+	this.nome = nome;
+}
+
+public String getUsuario() {
+	return usuario;
+}
+
+public void setUsuario(String usuario) {
+	this.usuario = usuario;
+}
+
+public String getSenha() {
+	return senha;
+}
+
+public void setSenha(String senha) {
+	this.senha = senha;
+}
+
+public LocalDate getDataNascimento() {
+	return dataNascimento;
+}
+
+public void setDataNascimento(LocalDate dataNascimento) {
+	this.dataNascimento = dataNascimento;
+}
+
+public List<Postagem> getPostagem() {
+	return postagem;
+}
+
+public void setPostagem(List<Postagem> postagem) {
+	this.postagem = postagem;
+}
+
 }
